ddf64413baa707b58c61a1fe553d2af546b00343
igor-derevyanchenko <igor.b.derevyanchenko@gmail.com>
2023-05-12 19:12:21 -0400

    blah

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   return (
     <div className="m-2">
-      <div className="navbar bg-base-300 rounded-xl">
+      <div className="navbar bg-secondary rounded-xl">
         <div className="navbar-start">
           <Link className="link-primary text-4xl" href="/">
             Igor&apos;s Anime List
