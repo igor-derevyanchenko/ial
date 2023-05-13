38d110efc4c3713cede27dae72714eda2e5a7f68
igor-derevyanchenko <igor.b.derevyanchenko@gmail.com>
2023-05-13 15:59:23 -0400

    is the issue mapping the list???

--- app/components/AniGrid.tsx
+++ app/components/AniGrid.tsx
@@ -21,13 +21,5 @@ export default async function AniGrid({ searchParams }: homeProps) {
     .then((res) => res.json())
     .then((parsedRes) => parsedRes.data);
 
-  return (
-    <div className="grid ani-grid gap-4 justify-center mt-4">
-      {aniList.map((item: any, index: number) => {
-        const anime = item.node;
-
-        return <div key={index}>{index}</div>;
-      })}
-    </div>
-  );
+  return <div className="grid ani-grid gap-4 justify-center mt-4">hello</div>;
 }
